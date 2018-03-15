e2415b513d338628efb769165405e0d8a0658790
Ruslan Bilyshko <ruslan.bilyshko@gmail.com>
2018-03-15 22:10:34 +0200

    Fix comment

--- modules/test/test.config.php
+++ modules/test/test.config.php
@@ -1,16 +1,16 @@
 <?php return [
+	
+	/**
+	 * Use config([module_name.foo]);
+	 */
+	
 	'foo' => 'bar',
 	
-	/*
-	|--------------------------------------------------------------------------
-	| Autoloaded Service Providers the module
-	|--------------------------------------------------------------------------
-	|
-	| The service providers listed here will be automatically loaded on the
-	| request to your application. Feel free to add your own services to
-	| this array to grant expanded functionality to your applications.
-	|
-	*/
+	/**
+	 * 	Autoloaded Service Providers the module
+	 * 
+	 * @var array
+	 */
 	'providers' => [
 		
 	],
