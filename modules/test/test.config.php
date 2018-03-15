38fe95f99b125dc8015de1c1e9d1e2252094c2ae
Ruslan Bilyshko <ruslan.bilyshko@gmail.com>
2018-03-15 22:07:18 +0200

    Add example keys in config test module

--- modules/test/test.config.php
+++ modules/test/test.config.php
@@ -1,3 +1,28 @@
 <?php return [
-		'foo' => 'bar'
+	'foo' => 'bar',
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Autoloaded Service Providers the module
+	|--------------------------------------------------------------------------
+	|
+	| The service providers listed here will be automatically loaded on the
+	| request to your application. Feel free to add your own services to
+	| this array to grant expanded functionality to your applications.
+	|
+	*/
+	'providers' => [
+		
+	],
+	
+	/**
+	 * The event listener mappings for the module.
+	 *
+	 * @var array
+	 */
+	'listeners' => [
+		
+	]
+	
+	
 ];
